0832cd1e81e9931fc63027735ef01f8d2e62fd9a
DianaQuispe <danaalienmas@gmail.com>
2017-09-19 12:31:14 -0500

    act

--- js/main.js
+++ js/main.js
@@ -1,35 +1,63 @@
 "use strict";  
 const app = {
+
     item :  {
     f :0,
-     comment: undefined
+    arreImages: new Array()
     },
     init : function() {
         $('#play').click(function() {
-            var arreImages = new Array();
-            
             $('#table').empty();
             var numTab = $('#inputTable').val( );
-            console.log(numTab);
             for(var i=0; i<numTab; i++) {
                 $('#table').append(`<tr>`);
                 for(var j=0; j<numTab; j++) {
-                    $('#table').append(`<td><img class="img" src="images/blanco.png" id=${app.item.f++} width="100" heigth="250" alt="">\
+                    $('#table').append(`<td><img class="img" src="images/blanco.png" id=${app.item.f++} alt="">\
                     </td></tr>`);
                 }
             }
-            $('img').click(function() {
-                $(this).attr("src","images/bomba.gif");
-                alert('d');
+            $('img').click(function(){
+                var ids = $(this).attr('id');
+                console.log($(this).attr('id'));
+                for(var i=0; i<app.item.arreImages.length; i++) {
+                    if(ids == app.item.arreImages[i]) {
+                        alert('perdiste');
+                        $(this).attr("src","images/bombita.jpg");
+                        $(this).addClass('bomba');
+
+                        
+                    }
+                    
+                }
+                
+                console.log(app.item.arreImages);    
+                $(this).attr("src","images/numero1.jpg");
                 
             })
+           
             
-          })
-
-    },
+    })
+    Array.prototype.unique=function(a){
+        return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+      });
+      app.setup();
+},
     setup: function() {
+        var numAl = parseInt($('#inputTable').val( ))+3; 
+        for(var i=0; i <parseInt($('#inputTable').val( ))+3;i ++){ 
+            var math =  Math.floor((Math.random() * 12));
+            app.item.arreImages.push(math);
+            app.item.arreImages =app.item.arreImages.unique();
+         
+            
+        }
+        if(app.item.arreImages.length != parseInt(numAl)) {
+            app.item.arreImages.push(math);
+        }
+        
 
+       
+    
     }
-
-}
+} 
 $(document).ready(app.init);

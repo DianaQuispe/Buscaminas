5644f40ed4a8adfe8f061098031e78e0d0870dfe
MyDiana <danaalienmas@gmail.com>
2017-09-19 07:25:06 -0500

    avance

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 "use strict";  
+const app = {
+    item :  {
+    f :0,
+     comment: undefined
+    },
+    init : function() {
+        $('#play').click(function() {
+            var arreImages = new Array();
+            
+            $('#table').empty();
+            var numTab = $('#inputTable').val( );
+            console.log(numTab);
+            for(var i=0; i<numTab; i++) {
+                $('#table').append(`<tr>`);
+                for(var j=0; j<numTab; j++) {
+                    $('#table').append(`<td><img class="img" src="images/blanco.png" id=${app.item.f++} width="100" heigth="250" alt="">\
+                    </td></tr>`);
+                }
+            }
+            $('img').click(function() {
+                $('img').src = "bomba.svg";
+                alert('d');
+                
+            })
+            
+          })
 
-$('#play').click(function() {
-    $('#table').empty();
-    var numTab = $('#inputTable').val( );
-    console.log(numTab);
-    for(var i=0; i<numTab; i++) {
-        $('#table').append(`<tr>`);
-        for(var j=0; j<numTab; j++) {
-            $('#table').append(`<td>dff</td></tr>`);
-        }
+    },
+    setup: function() {
+
     }
-})
-$('td').click(function(){
-    
-})
+
+}
+$(document).ready(app.init);
